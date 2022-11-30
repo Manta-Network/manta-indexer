3a0ccb2dfce0531fc185b0cbd23317f9527b09c0
Yi <flame0743@gmail.com>
2022-11-30 22:55:25 +0800

    add some log on stress-test

--- tests/integration-tests/src/stress-test.ts
+++ tests/integration-tests/src/stress-test.ts
@@ -1,6 +1,6 @@
 // The main idea is to test the performance of indexer.
 import {ApiPromise} from "@polkadot/api";
-import {dolphinFullNode, indexerAddress} from "./config.json";
+import {dolphinFullNode, dolphinFullNodeLocal, indexerAddress} from "./config.json";
 import createPromiseApi from "./utils";
 import {assert} from "chai";
 import {performance} from "perf_hooks";
@@ -10,28 +10,31 @@ describe("indexer stress test", function () {
     const receiver_shard_num = 256;
     const max_receiver_num = 1024;
     const max_sender_num = 1024;
+    const duration_sec_each_test_case = 60;
 
     let indexer_apis: Array<ApiPromise>;
     let full_node_apis: Array<ApiPromise>;
 
     before(async function () {
         indexer_apis = [];
+        full_node_apis = [];
         // initial batch of apis as request simulation pool.
         for (let i = 0; i < max_concurrent; ++i) {
-            // const indexer_api = await createPromiseApi(indexerAddress);
-            const indexer_api = await createPromiseApi(dolphinFullNode);
+            const indexer_api = await createPromiseApi(indexerAddress);
+            const full_node_api = await createPromiseApi(dolphinFullNodeLocal);
             const health = await indexer_api.rpc.system.health();
             assert.isNotTrue(health.toJSON().isSyncing);
             indexer_apis.push(indexer_api);
+            full_node_apis.push(full_node_api);
         }
     });
 
-    it("stress test", async function () {
+    it("baseline stress test", async function () {
         const total_receivers = 40000;
         const total_senders = 40000;
-        const test_duration_sec = 60;
 
         let query_latency: number[] = [];
+        let log_counter = 0;
 
         const once_pull_ledger_call = async function (api: ApiPromise) {
             const gen_random = function (min: number, max: number): number {
@@ -52,19 +55,22 @@ describe("indexer stress test", function () {
             );
             let cost = performance.now() - before;  // ms
             query_latency.push(cost);
-            console.log("get a response at cost(%i ms): %i, %i", cost, data.receivers.length, data.senders.length)
+            log_counter += 1;
+            if (log_counter % 1000 == 0) {
+                console.log("get a response at cost(%i ms): %i, %i", cost, data.receivers.length, data.senders.length)
+            }
         }
 
         let stop = false;
         setTimeout(function () {
             stop = true
-        }, test_duration_sec * 1000);
+        }, duration_sec_each_test_case * 1000);
 
         let count = 0;
         let task_queue = [];
         while (!stop) {
             for (let i = 0; i < max_concurrent; ++i) {
-                task_queue.push(once_pull_ledger_call(indexer_apis[i]));
+                task_queue.push(once_pull_ledger_call(full_node_apis[i]));
             }
             await Promise.all(task_queue);
             count += 1;
@@ -74,18 +80,18 @@ describe("indexer stress test", function () {
             return a - b;
         });
         let p99_latency = query_latency[Math.round(query_latency.length / 100 * 99)].toFixed(2);
-        let qps = (query_latency.length / test_duration_sec).toFixed(2);
+        let qps = (query_latency.length / duration_sec_each_test_case).toFixed(2);
         let avg_latency = (query_latency.reduce((acc, val) => acc + val, 0) / query_latency.length).toFixed(2);
-        console.log("stress test finish %d loop, qps = %d, avg = %d ms, p99 = %d ms", count, qps, avg_latency, p99_latency)
+        console.log("baseline stress test finish %d loop, qps = %d, avg = %d ms, p99 = %d ms", count, qps, avg_latency, p99_latency)
     })
 
 
-    it("dense stress test", async function () {
+    it("optimized dense stress test", async function () {
         const total_receivers = 5000000;
         const total_senders = 5000000;
-        const test_duration_sec = 60;
 
         let query_latency: number[] = [];
+        let log_counter = 0;
 
         const once_pull_ledger_call = async function (api: ApiPromise) {
             const gen_random = function (min: number, max: number): number {
@@ -106,13 +112,16 @@ describe("indexer stress test", function () {
             );
             let cost = performance.now() - before;  // ms
             query_latency.push(cost);
-            console.log("get a response at cost(%i ms): %i, %i", cost, data.receiver_len, data.sender_len)
+            log_counter += 1;
+            if (log_counter % 1000 == 0) {
+                console.log("get a response at cost(%i ms): %i, %i", cost, data.receiver_len, data.sender_len)
+            }
         }
 
         let stop = false;
         setTimeout(function () {
             stop = true
-        }, test_duration_sec * 1000);
+        }, duration_sec_each_test_case * 1000);
 
         let count = 0;
         let task_queue = [];
@@ -128,12 +137,10 @@ describe("indexer stress test", function () {
             return a - b;
         });
         let p99_latency = query_latency[Math.round(query_latency.length / 100 * 99)].toFixed(2);
-        let qps = (query_latency.length / test_duration_sec).toFixed(2);
+        let qps = (query_latency.length / duration_sec_each_test_case).toFixed(2);
         let avg_latency = (query_latency.reduce((acc, val) => acc + val, 0) / query_latency.length).toFixed(2);
         console.log("stress test finish %d loop, qps = %d, avg = %d ms, p99 = %d ms", count, qps, avg_latency, p99_latency)
     })
-
-
 
     it.skip("stress test for connection", async function () {
         // create a lot of client to connect and disconnect.
